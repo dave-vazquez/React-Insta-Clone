e2bd53a1b73716ec6f904fadf75a4e407cfe2a1b
dmvazli <davidmvazquez1@gmail.com>
2019-06-03 20:54:37 -0400

    build

---
+++ instagram/build/precache-manifest.f381f693483048ac57f45f1696edf110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ecf02fd4cd944cdeea8689979da4df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a41ee4faca7c78d536d2",
+    "url": "/static/css/main.7a727bda.chunk.css"
+  },
+  {
+    "revision": "337e69dd4c23108156ad",
+    "url": "/static/js/2.e55ed627.chunk.js"
+  },
+  {
+    "revision": "a41ee4faca7c78d536d2",
+    "url": "/static/js/main.b1badd78.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6027348bcdfb038ce4f9ba8f6906713a",
+    "url": "/static/media/instagram.6027348b.png"
+  }
+]);
